561a052b5c26c7f88283dc032b5504d9dea89626
Ta Chi Thanh Danh <22120049@student.hcmus.edu.vn>
2024-09-27 21:30:32 +0700

    Add User Controllers and User DTOs

---
+++ shopapp/src/main/java/com/example/shopapp/dtos/UserDTO.java
@@ -0,0 +1,44 @@
+package com.example.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.sql.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDTO {
+    @JsonProperty("full_name")
+    private String fullName;
+
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    private String address;
+
+    @NotBlank(message = "Password cannot be blank")
+    private String password;
+
+    @JsonProperty("retype_password")
+    private String retypePassword;
+
+    @JsonProperty("date_of_birth")
+    private Date dateOfBirth;
+
+    @JsonProperty("facebook_account_id")
+    private String facebookAccountId;
+
+    @JsonProperty("google_account_id")
+    private String googleAccountId;
+
+    @JsonProperty("role_id")
+    @NotNull(message = "Role ID is required")
+    private Long roleId;
+}
---
+++ shopapp/src/main/java/com/example/shopapp/dtos/UserLoginDTO.java
@@ -0,0 +1,21 @@
+package com.example.shopapp.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserLoginDTO {
+    @JsonProperty("phone_number")
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
